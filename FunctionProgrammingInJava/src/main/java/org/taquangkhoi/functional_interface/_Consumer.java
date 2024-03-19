91bbb08aa8cd7cbcfce4788a086f8741c2f3ce96
Tạ Quang Khôi <khoi0941@gmail.com>
2024-03-19 16:52:28 +0700

    Learn about BiConsumer

--- FunctionProgrammingInJava/src/main/java/org/taquangkhoi/functional_interface/_Consumer.java
+++ FunctionProgrammingInJava/src/main/java/org/taquangkhoi/functional_interface/_Consumer.java
@@ -1,12 +1,21 @@
 package org.taquangkhoi.functional_interface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
     public static void main(String[] args) {
         greetCustomer(new Customer("Khoi", "12345"));
         greetCustomerConsumer.accept(new Customer("Khac", "190412"));
+
+        greetCustomerConsumerV2.accept(new Customer("Khoi", "12345"), false);
     }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 =
+            (customer, showPhoneNumber) -> System.out.println("Hello " + customer.name +
+                                                              ", thanks for registering phone number "
+                                                              + (showPhoneNumber ? customer.phoneNumber : "********")
+            );
 
     static Consumer<Customer> greetCustomerConsumer =
             customer -> System.out.println("Hello " + customer.name +
