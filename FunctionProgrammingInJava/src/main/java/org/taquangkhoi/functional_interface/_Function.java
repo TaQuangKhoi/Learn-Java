2e89de8162cc524230176062ec01d37d852affe0
Tạ Quang Khôi <khoi0941@gmail.com>
2024-03-19 16:17:14 +0700

    Learn about BiFunction

--- FunctionProgrammingInJava/src/main/java/org/taquangkhoi/functional_interface/_Function.java
+++ FunctionProgrammingInJava/src/main/java/org/taquangkhoi/functional_interface/_Function.java
@@ -1,9 +1,11 @@
 package org.taquangkhoi.functional_interface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
+        // Function
         int increment = increment(2);
 //        System.out.println(increment);
 
@@ -13,8 +15,13 @@ public class _Function {
         int multiplyBy10 = multiplyBy10Function.apply(2);
 //        System.out.println(multiplyBy10);
 
+        // Function takes 1 argument and produces 1 result
         Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
-        System.out.println(addBy1AndThenMultiplyBy10.apply(2));
+//        System.out.println(addBy1AndThenMultiplyBy10.apply(2));
+
+        // BiFunction takes 2 arguments and produces 1 result
+        System.out.println(integerIntegerIntegerBiFunction.apply(4, 100));
+        System.out.println(incrementAndMultiply(4, 100));
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
@@ -23,5 +30,12 @@ public class _Function {
 
     static int increment(int number) {
         return number + 1;
+    }
+
+    static BiFunction<Integer, Integer, Integer> integerIntegerIntegerBiFunction =
+            (numberToIncrementBy, numToMultiply) -> (numberToIncrementBy + 1) * numToMultiply;
+
+    static int incrementAndMultiply(int number, int numToMultiply) {
+        return (number + 1) * numToMultiply;
     }
 }
